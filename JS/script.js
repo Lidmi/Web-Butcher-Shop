b9029b1df9e3ab7556bbbcd41950a7cc283b3119
Petroshenok Lidiia <lidiapetroshenok@gmail.com>
2023-04-05 16:29:39 +0300

    Add new repair section

--- JS/script.js
+++ JS/script.js
@@ -1,61 +1,69 @@
 let products = document.querySelectorAll('.product');
+AddDescriptions(products, true);
+let services = document.querySelectorAll('.repair');
+AddDescriptions(services, false);
+OpenCloseDescription(products);
+OpenCloseDescription(services);
 
- for (let i = 0; i < products.length; i++) {
-
-    let description_container = document.createElement("div");
-    description_container.className = "description-container";
-
-    let description = document.createElement("div");
-    description.className = "description";
-    products[i].appendChild(description_container);
-    description_container.appendChild(description);
-
-    let img = products[i].querySelector('.img').cloneNode();
-    let name = products[i].querySelector('.name').cloneNode(true);
-    let price = products[i].querySelector('.price').cloneNode(true);
-    let text = document.createElement("p");
-    let btn = document.createElement("button");
-    btn.className ="close  fa-solid fa-xmark";
-    text.textContent ='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, voluptas quisquam animi magnam laborum at magni corrupti ratione reprehenderit mollitia quod. Quaerat cumque commodi maxime!';
-
-    let img_and_price = document.createElement("div");
-    img_and_price.className = "img-and-price";
-
-    description.appendChild(name);
-    description.appendChild(btn);
-    description.appendChild(text);
-
-    img_and_price.appendChild(img);
-    img_and_price.appendChild(price);
-
-    description.appendChild(img_and_price);
-
-     let card = products[i].querySelector('.card');
-
-    //  card.onclick = () =>{
-    //       description_container.classList.add('active');
-    //  }
-
- }
-
- document.addEventListener('click', (e) => {
-    let card;
-    let path;
+function AddDescriptions(products, needImage) {
     for (let i = 0; i < products.length; i++) {
-        card = products[i].querySelector('.card');
-        description_container = products[i].querySelector('.description-container');
-        description = description_container.querySelector('.description');
-        path1 = e.composedPath().includes(card);
-        path2 = e.composedPath().includes(description_container);
-        path3 = e.composedPath().includes(description);
-        if (path1) {
-            description_container.classList.add('active');
-        } else if (path2 && !path3) {
-            description_container.classList.remove('active');
-        }
-    }
 
- })
+        let description_container = document.createElement("div");
+        description_container.className = "description-container";
+    
+        let description = document.createElement("div");
+        description.className = "description";
+        products[i].appendChild(description_container);
+        description_container.appendChild(description);
+
+        let name = products[i].querySelector('.name').cloneNode(true);
+        let price = products[i].querySelector('.price').cloneNode(true);
+        let text = document.createElement("p");
+        let btn = document.createElement("button");
+        btn.className ="close  fa-solid fa-xmark";
+        text.textContent ='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, voluptas quisquam animi magnam laborum at magni corrupti ratione reprehenderit mollitia quod. Quaerat cumque commodi maxime!';
+    
+        description.appendChild(name);
+        description.appendChild(btn);
+        description.appendChild(text);
+
+        if (needImage) {
+            let img = products[i].querySelector('.img').cloneNode();
+            let img_and_price = document.createElement("div");
+            img_and_price.className = "img-and-price";
+    
+            img_and_price.appendChild(img);
+            img_and_price.appendChild(price);
+        
+            description.appendChild(img_and_price);
+        } else {
+            description.appendChild(price);
+        }
+     }
+}
+
+function OpenCloseDescription(products) {
+    document.addEventListener('click', (e) => {
+        let card;
+        let path1; let path2; let path3;
+        let description_container; let description;
+        for (let i = 0; i < products.length; i++) {
+            card = products[i].querySelector('.card');
+            description_container = products[i].querySelector('.description-container');
+            description = description_container.querySelector('.description');
+            path1 = e.composedPath().includes(card);
+            path2 = e.composedPath().includes(description_container);
+            path3 = e.composedPath().includes(description);
+            if (path1) {
+                description_container.classList.add('active');
+            } else if (path2 && !path3) {
+                description_container.classList.remove('active');
+            }
+        }
+    
+     })
+}
+
 
 let close_btn = document.querySelectorAll('.close');
 for (let i = 0; i < close_btn.length; i++) {
